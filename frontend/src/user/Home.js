411f003189a82d61f95266c2d1984e6127f38369
utkarsh-UK <utkarshkore98@gmail.com>
2021-04-10 13:09:49 +0530

    Setup front end with react

---
+++ frontend/src/user/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-bold">Hello, Home</h1>
+    </div>
+  );
+};
+
+export default Home;
